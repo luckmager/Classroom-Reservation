2c0f784b26c3f9cc62cfe6ab1ec59d2129d57421
Jeremy Pietersz <galejeerd@hotmail.com>
2018-05-28 10:01:28 +0200

    Calendar view without reservations and Show all classrooms at /buildings/building.name

--- app/helpers/classrooms_helper.rb
+++ app/helpers/classrooms_helper.rb
@@ -13,4 +13,14 @@ module ClassroomsHelper
 		base64_output = Base64.encode64(barcode.to_png({ xdim: 5 }))
 		"data:image/png;base64,#{base64_output}"
 	end
+
+	def current_week
+		date = Date.today.beginning_of_week
+		return (date..date + 4.days).to_a
+	end
+
+	def next_week
+		date = Date.today.beginning_of_week + 7.days
+		return (date..date + 4.days).to_a
+	end
 end
