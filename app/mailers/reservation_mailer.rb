faabd7365fe9fa32e1d0f6eb7cbdd40abfb4c967
Jeremy Pietersz <galejeerd@hotmail.com>
2018-06-17 01:26:59 +0200

    More pretty mailer

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,7 +1,6 @@
 class ReservationMailer < ApplicationMailer
   def reservation_booked_mail
     @reservation = params[:reservation]
-    @user = User.find(@reservation.user_id)
-    mail(to: @user.email, subject: 'You have booked a reservation!')
+    mail(to: @reservation.user.email, subject: 'You have booked a reservation!')
   end
 end
