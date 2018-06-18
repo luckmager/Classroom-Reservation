341a285d54907fce1c47bae20545b61a1991de48
Jeremy Pietersz <galejeerd@hotmail.com>
2018-06-18 13:51:19 +0200

    Removed sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :reservations
-  devise_for :users, :controllers => { :registrations => "registrations", :sessions => "sessions" }
+  devise_for :users, :controllers => { :registrations => "registrations" }
   resources :devices
   resources :options
   resources :buildings
