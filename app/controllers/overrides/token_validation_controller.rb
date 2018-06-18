7fae75d64ba687edd87e43a81320709d7762e56d
Jeremy Pietersz <galejeerd@hotmail.com>
2018-06-18 15:46:04 +0200

    JSON response authentication

--- app/controllers/overrides/token_validation_controller.rb
+++ app/controllers/overrides/token_validation_controller.rb
@@ -5,13 +5,15 @@ module Overrides
       # @resource will have been set by set_user_by_token concern
       if @resource
         render json: {
-            data: @resource.as_json(methods: :calculate_operating_thetan)
+            success: true,
+            status: 200
         }
       else
         render json: {
             success: false,
-            errors: ["Invalid login credentials"]
-        }, status: 401
+            errors: ["Invalid login credentials"],
+            status: 401
+        }
       end
     end
   end
